77972eb24bce465e3f8f68c1ac9adfe8e10c040d
Thomas Scott <thomas@entagen.com>
2012-11-18 17:08:47 -0600

    UIApplication deleted.

--- core/crawler.cpp
+++ core/crawler.cpp
@@ -10,35 +10,10 @@ make crawl function to eliminate main
 #include <urlmon.h>
 #include <vector>
 #include <fstream>
+#include "Crawler.h"
 #pragma comment(lib, "urlmon.lib")
 using namespace std;
 
-class Crawler
-{
-public:
-	char * startUrl;
-	vector<char*> queue;
-	vector<char*> doneQueue;
-	vector<char*> content;
-	vector<char*> blacklistedDomains;
-	vector<char*> allowedDomains;
-	double maxPageCount;
-	// getting and setting functions
-	double getPageCount();
-	void setPageCount(double d);
-	char* getStartUrl();
-	void setStartUrl(char * c);
-	void addBlacklist(char * c);
-	void addAllowed(char * c);
-	vector<char*>& getBlacklisted();
-	vector<char*>& getAllowed();
-	// crawling functions
-	void download(char * c);
-	void check(char * c);
-	void crawl(char * start, vector<char*> blacklist, vector<char*> allowed, double max);
-	bool notBlacklisted(char * c);
-	bool allowed(char * c);
-};
 double Crawler::getPageCount()
 {
 	return maxPageCount;
