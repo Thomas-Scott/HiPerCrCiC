9e6852a002b144027404f1935b2bf24d104811f4
Maggie Wanek <mwanek4@gmail.com>
2012-11-22 01:59:33 -0600

    fixing stuff

--- README.md
+++
@@ -1,4 +0,0 @@
-HiPerCrCiC
-==========
-
-Simple pluggable crawler that works in conjunction with HiPerCiC developed as CS251 final project by Michael Taufen, Maggie Wanek, and Thomas Scott.
--- core/crawler.cpp
+++ core/crawler.cpp
@@ -1,19 +1,44 @@
+//thomas leave my crap alone!! I like things in one file while I work, will split up later. <3
+
 /* download to file name
 check domain as substring
 fix bug in allowed
-test download
-check max page count
-make crawl function to eliminate main
+test download and crawl
 */
 
 #include <iostream>
 #include <urlmon.h>
 #include <vector>
 #include <fstream>
-#include "Crawler.h"
 #pragma comment(lib, "urlmon.lib")
 using namespace std;
 
+class Crawler
+{
+public:
+	char * startUrl;
+	vector<char*> queue;
+	vector<char*> doneQueue;
+	vector<char*> content;
+	vector<char*> blacklistedDomains;
+	vector<char*> allowedDomains;
+	double maxPageCount;
+	// getting and setting functions
+	double getPageCount();
+	void setPageCount(double d);
+	char* getStartUrl();
+	void setStartUrl(char * c);
+	void addBlacklist(char * c);
+	void addAllowed(char * c);
+	vector<char*>& getBlacklisted();
+	vector<char*>& getAllowed();
+	// crawling functions
+	void download(char * c);
+	void check(char * c);
+	void crawl(char * start, vector<char*> blacklist, vector<char*> allowed, double max);
+	bool notBlacklisted(char * c);
+	bool allowed(char * c);
+};
 double Crawler::getPageCount()
 {
 	return maxPageCount;
