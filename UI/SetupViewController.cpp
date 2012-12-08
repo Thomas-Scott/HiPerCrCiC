0ec21869ca80c5d27073f8bd6be4e85db1244103
Michael Taufen <mtaufen@gmail.com>
2012-12-08 17:06:52 -0600

    Significant Setup UI Fixes

--- UI/SetupViewController.cpp
+++ UI/SetupViewController.cpp
@@ -12,34 +12,41 @@ SetupViewController::SetupViewController() : ViewController()
 
 
   _jobName = new TitledTextInputView("Job Name:");
-  _jobName->setBounds(CGRect(10,10,400,100));
+  _jobName->setBounds(CGRect(10,10,250,70));
+  getMasterView()->addSubView(_jobName);
 
   _downloadDir = new TitledTextInputView("Download Dir:");
-  _downloadDir->setBounds(CGRect(420,10,400,100));
+  _downloadDir->setBounds(CGRect(270,10,250,70));
+  getMasterView()->addSubView(_downloadDir);
 
   _startPage = new TitledTextInputView("Start Page:");
-  _startPage->setBounds(CGRect(500,220,400,100));
+  _startPage->setBounds(CGRect(530,10,400,70));
+  getMasterView()->addSubView(_startPage);
 
   _maxPageCount = new TitledTextInputView("Max Page Count:");
-  _maxPageCount->setBounds(CGRect(500,330,400,100));
+  _maxPageCount->setBounds(CGRect(730,330,180,70));
+  getMasterView()->addSubView(_maxPageCount);
   
-  _startJob = new Button("START", CGRect(700,700,200,50));
+  _startJob = new Button("START", CGRect(730,600,200,50));
+  _startJob->setOnClickCallback(&startJobButtonPressedWrapper, this);
+  getMasterView()->addSubView(_startJob);
 
   _aTitle = new TextRenderView();
   _aTitle->setBounds(CGRect(10,120,300,50));
-  _allowedDomains = new TextInputScrollView(CGRect(10,170,300,570));
+  _aTitle->setTextColor(CGColor(0,0,0,1));
+  _aTitle->setContent("Allowed Domains:");
+  getMasterView()->addSubView(_aTitle);
+  _allowedDomains = new TextInputScrollView(CGRect(10,170,350,400));
+  getMasterView()->addSubView(_allowedDomains);
 
   _bTitle = new TextRenderView();
-  _bTitle->setBounds(CGRect(140,120,300,50));
-  _blacklistedDomains = new TextInputScrollView(CGRect(140,480,300,570));
-
-
-  getMasterView()->addSubView(_allowedDomains);
+  _bTitle->setBounds(CGRect(370,120,300,50));
+  _bTitle->setTextColor(CGColor(0,0,0,1));
+  _bTitle->setContent("Blacklisted Domains:");
+  getMasterView()->addSubView(_bTitle);
+  _blacklistedDomains = new TextInputScrollView(CGRect(370,170,350,400));
   getMasterView()->addSubView(_blacklistedDomains);
-  getMasterView()->addSubView(_startPage);
-  getMasterView()->addSubView(_maxPageCount);
 
-  _startJob->setOnClickCallback(&startJobButtonPressedWrapper, this);
 }
 
 SetupViewController::~SetupViewController()
